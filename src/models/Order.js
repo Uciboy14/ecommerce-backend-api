bd0aa46045a848f7dcf3f514dc37b45b5dbb8ee0
uccodetech <uccodetech@gmail.com>
2024-08-09 12:56:51 -0400

    models/Order.js

---
+++ src/models/Order.js
@@ -0,0 +1,40 @@
+// src/models/Order.js
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentStatus: {
+    type: String,
+    enum: ['pending', 'completed', 'failed'],
+    default: 'pending',
+  },
+  orderStatus: {
+    type: String,
+    enum: ['processing', 'shipped', 'delivered', 'cancelled'],
+    default: 'processing',
+  },
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
